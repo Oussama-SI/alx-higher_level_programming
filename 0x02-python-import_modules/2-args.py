3e1904b41e6f86dc444a50872c35b4bf60b16470
oussama BOURROUS <oussama18bourrous@gmail.com>
2023-11-30 13:22:11 -0800

    try

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if len(argv) == 1:
     print("{} arguments.".format(len(argv)-1))
 elif len(argv) == 2:
     print("{} argument:".format(len(argv)-1))
-    print("{}: {}".format(len(argv)-1,argv[1]))
+    print("{}: {}".format(len(argv)-1, argv[1]))
 else:
     print("{} arguments:".format(len(argv)-1))
     for i in range(len(argv)-1):
-        print("{}: {}".format(i+1,argv[i+1]))
+        print("{}: {}".format(i+1, argv[i+1]))
