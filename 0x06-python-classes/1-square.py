1b1965cd295f1c884846b722c01eeec5481c2c8c
oussama BOURROUS <oussama18bourrous@gmail.com>
2024-03-23 16:04:40 -0700

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""default module"""
+
+
+class Square:
+    """myClass declared"""
+    pass
+    def __init__(self,size):
+        self._square__size = size;
+        Exception
