3c378a4c164518a85eb8f2792224c5a73c578161
OUSSAMA BOURROUS <oussama18bourrous@gmail.com>
2024-07-24 22:14:22 +0100

    done

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const arr = ['C is fun','Python is cool','javaScript is amazing'];
+const arr = ['C is fun','Python is cool','JavaScript is amazing'];
 for(i = 0; i < 3; i++)
-  console.log(arr[i]);
+	console.log(arr[i]);
