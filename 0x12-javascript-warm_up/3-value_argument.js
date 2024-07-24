0cdad94d3e0e52bf27ef4a2be54e3b083878af47
root <OUSSAMA BOURROUS>
2024-07-24 06:21:03 -0700

    back to back

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const {argv} = require('node:process');
-	if (!argv[2]) {
-		console.log("No argument");
-	}
-	else{
-		console.log(argv[2]);
-	};
-
+const { argv } = require('node:process');
+if (!argv[2]) {
+  console.log('No argument');
+} else {
+  console.log(argv[2]);
+}
