907182f3648dfad679ebe840aece227cd4ecee9b
root <OUSSAMA BOURROUS>
2024-07-24 08:43:38 -0700

    done

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-
+if (isNaN(parseInt(argv[2])) || (argv[2] === undefined)) {
+  console.log('Not a number');
+} else {
+  console.log(parseInt(argv[2]));
+}
