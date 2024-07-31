150030f6963af4e636ea6148807b370d097d895c
OUSSAMA BOURROUS <oussama18bourrous@gmail.com>
2024-07-31 19:26:08 +0100

    done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-
+/*a script that prints x times “C is fun”*/
 if (isNaN(parseInt(argv[2])) || argv[2] === undefined) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < parseInt(argv[2]); i++) { console.log('C is fun'); }
+  for (let i = 0; i < parseInt(argv[2]); i++) { console.log("C is fun"); }
 }
