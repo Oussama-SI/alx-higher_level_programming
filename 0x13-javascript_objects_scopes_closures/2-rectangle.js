5e5eb2cd8b0caa0e6be53d7f601e08d8a967581f
OUSSAMA BOURROUS <oussama18bourrous@gmail.com>
2024-08-05 19:53:49 +0100

    done

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
-  width;
-  height;
   constructor (w, h) {
+    if (w <= 0 || h <= 0) return;
+    if (typeof w !== 'number' || typeof h !== 'number') return;
     this.width = w;
     this.height = h;
   }
 }
+
 module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Rectangle {
+  height;
+  width;
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) return;
+    if (isNaN(w) || isNaN(h)) return;
+    this.width = w;
+    this.height = h;
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
